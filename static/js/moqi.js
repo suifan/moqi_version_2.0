8017c3fc52cf8f52d1be673d34fb0c42c21badae
rickyscoder <rickyzhz@foxmail.com>
2017-06-05 13:28:16 +0800

    党建 左

--- static/js/moqi.js
+++ static/js/moqi.js
@@ -231,7 +231,7 @@
                 $("#rightSide").html(template('governmentTemp_right', {}));
                 chart.pieChart("secretaryChart", "#1b9deb", "#1b9deb", [{ "value": 100 }], '220', '人');
                 chart.pieChart("workTeamChart", "#1b9deb", "#1b9deb", [{ "value": 100 }], '55', "支");
-                chart.pieChart("cadreChart", "#1b9deb", "#1b9deb", [{ "value": 100 }], '440', '人');
+                chart.pieChart("cadreChart", "#1b9deb", "#1b9deb", [{ "value": 100 }], '1322', '人');
                 $('.bottom').html(template('governmentTemp_bottom', {}));
                 $(".command").viewer();
                 // bottomBind();
